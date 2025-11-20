d1bb188796eefb5cef29558cf72d3532c01b4533
Javen Goh <javengohzhengxian123@gmail.com>
2025-11-20 08:13:06 +0800

    Refactor authentication to use callback pattern matching template style

--- src/routers/User.router.js
+++ src/routers/User.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controller/userController');
+const { hashPassword, comparePassword } = require('../middleware/bcryptMiddleware');
+const { generateToken, sendToken } = require('../middleware/jwtMiddleware');
 const router = express.Router();
 
-router.post('/register', userController.register);
-router.post('/login', userController.login);
+router.post('/register', userController.checkEmailExist, hashPassword, userController.register, generateToken, sendToken);
+router.post('/login', userController.login, comparePassword, generateToken, sendToken);
 
 module.exports = router;
 

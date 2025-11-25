e6751384cab5280a6ef102b6cbf0a3d549de377f
boonhengg <bhkee1310@gmail.com>
2025-11-25 10:43:41 +0800

    fixed issue in userId

--- src/controller/dashboardController.js
+++ src/controller/dashboardController.js
@@ -1,8 +1,12 @@
 const dashboardModel = require("../models/dashboardModel");
 const { EMPTY_RESULT_ERROR } = require('../errors');
 
+// const userId = res.locals.userId; // TEMP: use real session user later
+
 module.exports.getUser = function (req, res) {
-  const userId = res.locals.userId; // Get user ID from JWT token
+  const userId = res.locals.userId;
+  console.log(userId,"get user")
+
   return dashboardModel.getCurrentUser(userId)
     .then(users => res.json({ users }))
     .catch(err => res.status(500).json({ error: err.message }));
@@ -11,6 +15,7 @@ module.exports.getUser = function (req, res) {
 
 module.exports.getDashboard = function (req, res) {
   const userId = res.locals.userId; // Get user ID from JWT token
+    console.log(userId,"get task from user")
   return dashboardModel.getDashboardStats(userId)
     .then(stats => res.json({ stats }))
     .catch(err => res.status(500).json({ error: err.message }));

f414c933545c1f33bd109a86b3c539bd76d75b96
TaeJunmatt <tjlmatthew06.24@ichat.sp.edu.sg>
2025-12-02 09:33:27 +0800

    Fix Error finding modules

--- src/models/categoriesModel.js
+++ src/models/categoriesModel.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const { EMPTY_RESULT_ERROR } = require("../errors/errors");
+const { EMPTY_RESULT_ERROR } = require("../errors");
 
 module.exports.getAllCategories = (userId) => {
     return prisma.category.findMany({
